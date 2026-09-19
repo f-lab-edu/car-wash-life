83f64558a3e5c49bb992676630a8477b9e87ac1d
nGyu <vpdls1511@gmail.com>
2026-09-19 21:34:31 +0900

    feat : 최초 커밋

---
+++ src/main/java/me/ngyu/carwashlife/CarWashLifeApplication.java
@@ -0,0 +1,13 @@
+package me.ngyu.carwashlife;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CarWashLifeApplication {
+
+  public static void main(String[] args) {
+    SpringApplication.run(CarWashLifeApplication.class, args);
+  }
+
+}
---
+++ src/test/java/me/ngyu/carwashlife/CarWashLifeApplicationTests.java
@@ -0,0 +1,13 @@
+package me.ngyu.carwashlife;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CarWashLifeApplicationTests {
+
+  @Test
+  void contextLoads() {
+  }
+
+}
